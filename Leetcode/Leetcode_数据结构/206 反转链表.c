c47e5ca3ea8d246b62cc10dca5464e8a96e42762
zhengyiq <601666966@qq.com>
2022-11-06 14:26:52 +0800

    11-06数据结构 - 链表

--- Leetcode/Leetcode_数据结构/206 反转链表.c
+++ Leetcode/Leetcode_数据结构/206 反转链表.c
@@ -11,3 +11,22 @@
 //    }
 //    return cur;
 //}
+
+
+//将链表指针翻转
+//struct ListNode* reverseList(struct ListNode* head) {
+//    struct ListNode* n1 = NULL;
+//    struct ListNode* n2 = head;
+//    if (head == NULL)
+//        return NULL;
+//    struct ListNode* n3 = n2->next;
+//    while (n2)
+//    {
+//        n2->next = n1;
+//        n1 = n2;
+//        n2 = n3;
+//        if (n3)
+//            n3 = n3->next;
+//    }
+//    return n1;
+//}
