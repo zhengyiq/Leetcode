e2252d865ae2d6491b3c4241c1379aa1d147d291
zhengyiq <601666966@qq.com>
2022-12-05 14:26:41 +0800

    12-05

--- 牛客/链表/树/KY11 二叉树遍历.c
+++ 牛客/链表/树/KY11 二叉树遍历.c
@@ -1,47 +1,47 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
-#include <stdlib.h>
-struct TreeNode {
-    char val;
-    struct TreeNode* left;
-    struct TreeNode* right;
-};
-
-
-struct TreeNode* TreeCreate(char* arr, int* pi)
-{
-    if (arr[*pi] == '#')
-    {
-        (*pi)++;
-        return NULL;
-    }
-
-    struct TreeNode* newnode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
-
-    newnode->val = arr[(*pi)++];
-
-    newnode->left = TreeCreate(arr, pi);
-    newnode->right = TreeCreate(arr, pi);
-
-    return newnode;
-}
-
-void InOrder(struct TreeNode* root)
-{
-    if (root == NULL)
-    {
-        return;
-    }
-    InOrder(root->left);
-    printf("%c ", root->val);
-    InOrder(root->right);
-}
-
-int main() {
-    char arr[100];
-    scanf("%s", arr);
-    int pi = 0;
-    struct TreeNode* root = TreeCreate(arr, &pi);
-    InOrder(root);
-    return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//#include <stdlib.h>
+//struct TreeNode {
+//    char val;
+//    struct TreeNode* left;
+//    struct TreeNode* right;
+//};
+//
+//
+//struct TreeNode* TreeCreate(char* arr, int* pi)
+//{
+//    if (arr[*pi] == '#')
+//    {
+//        (*pi)++;
+//        return NULL;
+//    }
+//
+//    struct TreeNode* newnode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+//
+//    newnode->val = arr[(*pi)++];
+//
+//    newnode->left = TreeCreate(arr, pi);
+//    newnode->right = TreeCreate(arr, pi);
+//
+//    return newnode;
+//}
+//
+//void InOrder(struct TreeNode* root)
+//{
+//    if (root == NULL)
+//    {
+//        return;
+//    }
+//    InOrder(root->left);
+//    printf("%c ", root->val);
+//    InOrder(root->right);
+//}
+//
+//int main() {
+//    char arr[100];
+//    scanf("%s", arr);
+//    int pi = 0;
+//    struct TreeNode* root = TreeCreate(arr, &pi);
+//    InOrder(root);
+//    return 0;
+//}
